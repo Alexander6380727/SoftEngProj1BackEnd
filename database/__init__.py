a8bf5db5fa181d0af81ba364c8674251ce47c7a6
Sasha <alexander.oga@student.mahidol.edu>
2025-02-02 21:48:20 +0700

    Render v1

--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 #DATABASE_URL = "postgresql+asyncpg://admin:adminpass@localhost:5432/labDB"
-DATABASE_URL = "postgresql+asyncpg://admin:adminpass@localhost:5432/labDB"
+#DATABASE_URL = "postgresql+asyncpg://admin:adminpass@localhost:5432/labDB"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
--- database/create_tables.py
+++ database/create_tables.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
+import os
 
 # Replace 'DATABASE_URL' with your actual database URL
-DATABASE_URL = "sqlite:///./test.db"  # Example for SQLite, change as needed
+#DATABASE_URL = "sqlite:///./test.db"  # Example for SQLite, change as needed
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(bind=engine)
